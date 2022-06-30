f88b1807c198fa29723882a5e431957fab00d036
Ridhvi Kulshrestha <ridhvikul07@gmail.com>
2022-06-30 14:37:58 +0530

    commit first

---
+++ power_of_number.cpp
@@ -0,0 +1,28 @@
+//to find the exponential without using the pow function using the binary exponential
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void power_num(int a,int b)
+{
+    int res = 1;
+    while(b)
+    {
+        if(b & 1) //checking the bits which are set or not
+         res *=a;  //if set then multiply the number a
+
+         a *=a; //if not then just multiply
+         b = b >> 1; //right shift is done to remove the bits one by one from the right hand side of a binary number
+    }
+
+    cout<<res;
+}
+int main()
+{
+    int a,b;
+
+    cin>>a>>b;
+
+    power_num(a,b);
+}
